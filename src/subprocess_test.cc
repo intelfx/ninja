d94bfcf5fc0091a13f89d51e303ef8ba7313472c
Stefan Becker <stefanb@gpartner-nvidia.com>
2016-05-27 16:47:10 +0300

    Add TokenPool to SubprocessSet::DoWork()
    - add monitor interface to TokenPool
    - posix: monitor token rfd_ in DoWork() too

--- src/subprocess_test.cc
+++ src/subprocess_test.cc
@@ -47,7 +47,7 @@ TEST_F(SubprocessTest, BadCommandStderr) {
 
   while (!subproc->Done()) {
     // Pretend we discovered that stderr was ready for writing.
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   EXPECT_EQ(ExitFailure, subproc->Finish());
@@ -61,7 +61,7 @@ TEST_F(SubprocessTest, NoSuchCommand) {
 
   while (!subproc->Done()) {
     // Pretend we discovered that stderr was ready for writing.
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   EXPECT_EQ(ExitFailure, subproc->Finish());
@@ -79,7 +79,7 @@ TEST_F(SubprocessTest, InterruptChild) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   EXPECT_EQ(ExitInterrupted, subproc->Finish());
@@ -90,7 +90,7 @@ TEST_F(SubprocessTest, InterruptParent) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    bool interrupted = subprocs_.DoWork();
+    bool interrupted = subprocs_.DoWork(NULL);
     if (interrupted)
       return;
   }
@@ -103,7 +103,7 @@ TEST_F(SubprocessTest, InterruptChildWithSigTerm) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   EXPECT_EQ(ExitInterrupted, subproc->Finish());
@@ -114,7 +114,7 @@ TEST_F(SubprocessTest, InterruptParentWithSigTerm) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    bool interrupted = subprocs_.DoWork();
+    bool interrupted = subprocs_.DoWork(NULL);
     if (interrupted)
       return;
   }
@@ -127,7 +127,7 @@ TEST_F(SubprocessTest, InterruptChildWithSigHup) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   EXPECT_EQ(ExitInterrupted, subproc->Finish());
@@ -138,7 +138,7 @@ TEST_F(SubprocessTest, InterruptParentWithSigHup) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    bool interrupted = subprocs_.DoWork();
+    bool interrupted = subprocs_.DoWork(NULL);
     if (interrupted)
       return;
   }
@@ -154,7 +154,7 @@ TEST_F(SubprocessTest, Console) {
     ASSERT_NE((Subprocess*)0, subproc);
 
     while (!subproc->Done()) {
-      subprocs_.DoWork();
+      subprocs_.DoWork(NULL);
     }
 
     EXPECT_EQ(ExitSuccess, subproc->Finish());
@@ -168,7 +168,7 @@ TEST_F(SubprocessTest, SetWithSingle) {
   ASSERT_NE((Subprocess *) 0, subproc);
 
   while (!subproc->Done()) {
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
   ASSERT_EQ(ExitSuccess, subproc->Finish());
   ASSERT_NE("", subproc->GetOutput());
@@ -203,7 +203,7 @@ TEST_F(SubprocessTest, SetWithMulti) {
   while (!processes[0]->Done() || !processes[1]->Done() ||
          !processes[2]->Done()) {
     ASSERT_GT(subprocs_.running_.size(), 0u);
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
 
   ASSERT_EQ(0u, subprocs_.running_.size());
@@ -238,7 +238,7 @@ TEST_F(SubprocessTest, SetWithLots) {
     procs.push_back(subproc);
   }
   while (!subprocs_.running_.empty())
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   for (size_t i = 0; i < procs.size(); ++i) {
     ASSERT_EQ(ExitSuccess, procs[i]->Finish());
     ASSERT_NE("", procs[i]->GetOutput());
@@ -255,7 +255,7 @@ TEST_F(SubprocessTest, SetWithLots) {
 TEST_F(SubprocessTest, ReadStdin) {
   Subprocess* subproc = subprocs_.Add("cat -");
   while (!subproc->Done()) {
-    subprocs_.DoWork();
+    subprocs_.DoWork(NULL);
   }
   ASSERT_EQ(ExitSuccess, subproc->Finish());
   ASSERT_EQ(1u, subprocs_.finished_.size());
